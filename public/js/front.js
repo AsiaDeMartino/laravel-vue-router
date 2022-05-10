fb37bb7406ee36fcd67a09d7c40807ecf16327d8
asia_demartino@libero.it <asia_demartino@libero.it>
2022-05-10 20:05:16 +0200

    installato vue router

--- public/js/front.js
+++ public/js/front.js
@@ -15775,7 +15775,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/asiademartino/Desktop/boolean/esercizi/laravel-api/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/asiademartino/Desktop/boolean/esercizi/laravel-vue-router/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
